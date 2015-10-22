31e1c8beedb6a09da192471ad30da6fd692a610a
ibanezz <ibanezz@126.com>
2015-10-22 22:31:28 +0800

    完成文字本地化

--- OCTemplet/ViewController.m
+++ OCTemplet/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *content = [NSString stringWithFormat:NSLocalizedString(@"KEY", nil), @(1000000)];
+    NSLog(@"%@", content);
+    
 }
 
 - (void)didReceiveMemoryWarning {
